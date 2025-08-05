098c1b14490a9de53dd3a7823bf1d9136f13ee6a
QAIS-ALRUBBI <qaisalrubbi1@outlook.com>
2025-08-05 17:24:03 +0300

    This is code Explain what the Type of CAR

--- carType.py
+++ carType.py
@@ -1,4 +1,36 @@
 #This code know you Types of Car 
 
-print("BMW " \
-"       tOYOTA")
+print("""🚗 By Body Style
+Type	Description	Example
+Sedan	4-door car with a separate trunk	Toyota Camry, Honda Accord
+Hatchback	4 or 5-door with a rear hatch instead of a trunk	VW Golf, Ford Focus
+SUV (Sport Utility Vehicle)	High ground clearance, can be 2WD or 4WD	Toyota RAV4, Ford Explorer
+Crossover (CUV)	Built on a car platform, like a smaller SUV	Honda CR-V, Nissan Rogue
+Coupe	2-door car, sporty design	BMW 2 Series, Ford Mustang
+Convertible (Cabriolet)	Has a retractable roof	Mazda MX-5 Miata, BMW Z4
+Pickup Truck	Open cargo area in the back	Ford F-150, Toyota Hilux
+Minivan (MPV)	Family car with sliding doors	Honda Odyssey, Toyota Sienna
+Wagon (Estate)	Like a sedan but with an extended rear cargo area	Volvo V60, Audi A4 Allroad
+
+⚙️ By Drivetrain / Performance
+Type	Description	Example
+AWD (All-Wheel Drive)	Power to all 4 wheels automatically	Subaru Outback
+4WD (Four-Wheel Drive)	Manual or automatic engagement for tough terrain	Jeep Wrangler
+FWD (Front-Wheel Drive)	Power to front wheels (common in most cars)	Hyundai Elantra
+RWD (Rear-Wheel Drive)	Power to rear wheels, common in sporty/luxury cars	BMW 3 Series
+
+⚡ By Fuel Type
+Type	Description	Example
+Gasoline (Petrol)	Most common internal combustion engine	Toyota Corolla
+Diesel	More torque and fuel-efficient	Ford Ranger Diesel
+Hybrid	Mix of gas engine + electric motor	Toyota Prius
+Plug-in Hybrid (PHEV)	Can be plugged in to charge electric battery	Mitsubishi Outlander PHEV
+Electric (EV)	Fully electric, no gas engine	Tesla Model 3, Nissan Leaf
+
+🏎️ By Purpose or Class
+Type	Description	Example
+Luxury	High-end materials and features	Mercedes-Benz S-Class
+Sports Car	Designed for speed and performance	Porsche 911
+Supercar / Hypercar	Extremely fast and expensive	Lamborghini Aventador, Bugatti Chiron
+Off-Road	Built for tough terrain	Land Rover Defender
+Commercial / Fleet	Used for cargo or business	Ford Transit, Ram ProMaster""")
